50886b37d811913e1ef6a6a56b09d6f88d8ae42c
Agata Woźniak <Agata.Wozniak@rossmann.com.pl>
2022-09-27 11:48:56 +0200

    Add project files.

---
+++ Workshop26.Test/CalculatorTest.cs
@@ -0,0 +1,89 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Workshop26.Test
+{
+    public class CalculatorTest
+    {
+
+        [TestCase("")]
+        public void ShouldCalcEmptyStrings(string numbers)
+        {
+            var result = new Calculator();
+            Assert.That(result.Add(numbers), Is.EqualTo(0));
+        }
+
+        [TestCase(null)]
+        public void ShouldCalcNullStrings(string numbers)
+        {
+            var result = new Calculator();
+            Assert.That(result.Add(numbers), Is.EqualTo(0));
+        }
+
+        [TestCase("0")]
+        public void ShouldCalcZeroStrings(string numbers)
+        {
+            var result = new Calculator();
+            Assert.That(result.Add(numbers), Is.EqualTo(0));
+        }
+
+        [TestCase("1")]
+        public void ShouldCalcNumStrings(string numbers)
+        {
+            var result = new Calculator();
+            Assert.That(result.Add(numbers), Is.EqualTo(1));
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("3,7,2,1", 13)]
+        public void ShouldCalcManyNumbersStrings(string numbers, int result)
+        {
+            var res = new Calculator();
+            Assert.That(res.Add(numbers), Is.EqualTo(result));
+        }
+
+        [TestCase("1\n2, 3", 6)]
+        public void ShouldCalcManyNumbersAfterCommaStrings(string numbers, int result)
+        {
+            var res = new Calculator();
+            Assert.That(res.Add(numbers), Is.EqualTo(result));
+        }
+
+        [TestCase("//[;]\n1;2", 3)]
+        public void ShouldCalcManyNumbersWithDelimitersStrings(string numbers, int result)
+        {
+            var res = new Calculator();
+            Assert.That(res.Add(numbers), Is.EqualTo(result));
+        }
+
+        [TestCase("-1,-2", -3)]
+        public void ShouldCalcManyNumbersMinusStrings(string numbers, int result)
+        {
+            var res = new Calculator();
+
+            Assert.That(() => res.Add(numbers), Throws.TypeOf<ArgumentException>().With.Message.EqualTo("negatives not allowed"));
+        }
+
+        [TestCase("1000,2", 2)]
+        public void ShouldCalcManyNumbersWithDelimitersThousandsStrings(string numbers, int result)
+        {
+            var res = new Calculator();
+            Assert.That(res.Add(numbers), Is.EqualTo(result));
+        }
+
+        [TestCase("//[***]\n1***2", 3)]
+        public void ShouldCalcManyNumbersWithDelimitersLongStrings(string numbers, int result)
+        {
+            var res = new Calculator();
+            Assert.That(res.Add(numbers), Is.EqualTo(result));
+        }
+
+        //[TestCase("//[*][%]\n1*2%3", 6)]
+        //public void ShouldCalcManyNumbersWithDelimiters1Strings(string numbers, int result)
+        //{
+        //    var res = new Calculator();
+        //    Assert.That(res.Add(numbers), Is.EqualTo(result));
+        //}
+    }
+}
---
+++ Workshop26/Calculator.cs
@@ -0,0 +1,31 @@
+﻿namespace Workshop26
+{
+
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            if (String.IsNullOrEmpty(numbers))
+                return 0;
+
+            List<string> delimiter = new List<string>() { ",", "\n" };
+            if (numbers.StartsWith("//"))
+            {
+                var index = numbers.IndexOf(']');
+                delimiter.Add(numbers.Substring(3, index - 3));
+                numbers = numbers.Remove(0, index+1);
+            }
+            //"//[*][%]\n1*2%3"
+            var numberArrays = numbers.Split(delimiter.ToArray(), StringSplitOptions.None);
+
+            if (numberArrays.Where(x => !String.IsNullOrEmpty(x) && Int32.Parse(x) < 0).Count() > 0)
+                throw new ArgumentException("negatives not allowed");
+
+            var result = numberArrays.Where(x => !String.IsNullOrEmpty(x) && Int32.Parse(x) < 1000).Sum(x => Int32.Parse(x));
+            return result;
+        }
+
+
+    }
+
+}
